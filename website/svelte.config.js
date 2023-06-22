898fb29e08631e365941c8ca1d1ee26613842255
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-06-22 15:28:19 -0400

    fix?

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/adapter-vercel').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ runtime: 'nodejs18.x' })
 	}
 };
 
