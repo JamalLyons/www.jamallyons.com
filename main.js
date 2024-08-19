cf7a52ac75b5169ea22c351ff0f255a4fbe5f35d
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2024-08-19 16:39:53 -0400

    updates
    making the site more professional

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 // Loading scripts
-import "./public/js/social-links.js";
-import "./public/js/project-links.js";
-
 window.addEventListener("load", () => {
 	document.getElementById("loading-overlay").style.display = "none";
 });
