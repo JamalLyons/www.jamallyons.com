73e99802a88992f077120606dadb10e58713359c
Jamal Lyons <43446907+JamalLyons@users.noreply.github.com>
2025-04-07 11:45:46 -0400

    Update Blogs.tsx

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -8,20 +8,20 @@ import Particles from "./Particles";
 
 const blogs = [
   {
-    title: "Building Type-Safe Rust Applications with Convex",
-    excerpt:
-      "If you've been following the backend-as-a-service landscape, you've likely heard of Convex. This innovative platform has been turning heads by offering a unique combination of developer experience, serverless functions, and real-time subscriptions, all wrapped in a developer-friendly package. What makes Convex particularly interesting is that under the hood, it's powered by Rust – a language choice that speaks volumes about its commitment to performance and reliability.",
-    date: "2024-12-09",
-    readTime: "15 min read",
-    link: "https://stack.convex.dev/building-type-safe-rust-applications-with-convex",
-  },
-  {
     title: "Authentication Best Practices: Convex, Clerk and Next.js",
     excerpt:
       "Authentication is the backbone of any full-stack application, but it's also one of the easiest places to introduce subtle, hard-to-debug security flaws. As developers, we aim to build secure, reliable systems, but scaling an app to a global audience presents unique challenges—especially regarding authentication.",
     data: "2025-03-06",
     readTime: "25 min read",
     link: `https://stack.convex.dev/authentication-best-practices-convex-clerk-and-nextjs`,
+  },
+  {
+    title: "Building Type-Safe Rust Applications with Convex",
+    excerpt:
+      "If you've been following the backend-as-a-service landscape, you've likely heard of Convex. This innovative platform has been turning heads by offering a unique combination of developer experience, serverless functions, and real-time subscriptions, all wrapped in a developer-friendly package. What makes Convex particularly interesting is that under the hood, it's powered by Rust – a language choice that speaks volumes about its commitment to performance and reliability.",
+    date: "2024-12-09",
+    readTime: "15 min read",
+    link: "https://stack.convex.dev/building-type-safe-rust-applications-with-convex",
   },
 ];
 
