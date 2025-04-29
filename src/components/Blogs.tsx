ed2e2f114dffa66095cb66411655143ac17367f1
Jamal Lyons <43446907+JamalLyons@users.noreply.github.com>
2025-04-29 09:33:05 -0400

    Update Blogs.tsx

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -16,12 +16,19 @@ interface Blog {
 
 const blogs: Blog[] = [
   {
+    title: "Transactional Email in Convex with Bluefox and AWS SES",
+    excerpt: `Bluefox is a modern email API built on top of Amazon SES, designed for developers and SaaS teams. Whether you’re sending transactional emails or integrating real-time webhooks in a serverless Convex backend, Bluefox simplifies the process.`,
+    date: "2025-04-23",
+    readTime: "15 min read",
+    link: "https://dev.to/convexchampions/transactional-email-in-convex-with-bluefox-and-aws-ses-4kpc",
+  },
+  {
     title: "Authentication Best Practices: Convex, Clerk and Next.js",
     excerpt:
       "Authentication is the backbone of any full-stack application, but it's also one of the easiest places to introduce subtle, hard-to-debug security flaws. As developers, we aim to build secure, reliable systems, but scaling an app to a global audience presents unique challenges—especially regarding authentication.",
     date: "2025-03-06",
     readTime: "25 min read",
-    link: `https://stack.convex.dev/authentication-best-practices-convex-clerk-and-nextjs`,
+    link: `https://dev.to/convexchampions/authentication-best-practices-convex-clerk-and-nextjs-21ag`,
   },
   {
     title: "Building Type-Safe Rust Applications with Convex",
@@ -29,7 +36,7 @@ const blogs: Blog[] = [
       "If you've been following the backend-as-a-service landscape, you've likely heard of Convex. This innovative platform has been turning heads by offering a unique combination of developer experience, serverless functions, and real-time subscriptions, all wrapped in a developer-friendly package. What makes Convex particularly interesting is that under the hood, it's powered by Rust – a language choice that speaks volumes about its commitment to performance and reliability.",
     date: "2024-12-09",
     readTime: "15 min read",
-    link: "https://stack.convex.dev/building-type-safe-rust-applications-with-convex",
+    link: "https://dev.to/convexchampions/building-type-safe-rust-applications-with-convex-introducing-convex-typegen-1g38",
   },
 ];
 
